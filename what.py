a090843770039b332fa69f333b0efa7ab548e4cd
CynthiaTheGriffin <cxiong@wm.edu>
2024-10-24 12:29:38 -0400

    Co-authored-by: Dwight Smith <Allusionist8359@users.noreply.github.com> Co-authored-by: SSSafdar <SSSafdar@users.noreply.github.com>

--- what.py
+++ what.py
@@ -1,21 +1,28 @@
 import psycopg2
 
-# Connect to database
-conn = psycopg2.connect(dbname="what")
-cur = conn.cursor()
-
-
 ### Generate advisor list
 def gen_advisor_list():
+    # List the ID and name of every student along with the name of the student's advisor
     query = '''
-    SELECT *
-    FROM advisor;
-    '''
+    SELECT student.ID, student.name, instructor.name AS advisor
+    FROM student
+    LEFT JOIN advisor ON(advisor.s_ID = student.ID)
+    JOIN instructor ON(advisor.i_ID = instructor.ID)
+    ;'''
+    conn = psycopg2.connect(dbname="what")
+    cur = conn.cursor()
+
     cur.execute(query)
+    conn.close()
     return
 
 ### Hire new instructor
 def hire_instructor():
+    
+    # Connect to database
+    conn = psycopg2.connect(dbname="what")
+    cur = conn.cursor()
+        
     
     def prompts():
         fac = input("Enter the name of a new faculty member: ")
@@ -24,13 +31,13 @@ def hire_instructor():
         sal = input("Enter the faculty member's salary: ")
         return fac,fid,dep,sal
 
-    conn = psycopg2.connect(dbname="dbinstrux")
-    cur = conn.cursor()
-
     fac,fid,dep,sal = prompts()
 
     try:
-        cur.execute(query, (fid,fac,dep,sal))
+        query = '''
+        INSERT INTO instructor VALUES (%s,%s,%s,%s)
+        ;'''
+        cur.execute(query, (fid,fac,dep,sal)) # id, name, dept_name, salary
         conn.commit()
     except psycopg2.errors.UniqueViolation:
         print("ID already in use.")
@@ -60,23 +67,70 @@ def hire_instructor():
 
 ### Generate transcript
 def gen_transcript():
+    
+    # Connect to database
+    conn = psycopg2.connect(dbname="what")
+    cur = conn.cursor()
+        
     student_id = input("Enter a student ID: ")
+
+    query = '''
+    SELECT ID, name, dept_name
+    FROM student, 
+    ;'''
 
 ### Generate course list
 def gen_course_list():
+
+    # Connect to database
+    conn = psycopg2.connect(dbname="what")
+    cur = conn.cursor()
+        
     sem = input("Enter the semester: ")
     year = input("Enter the year: ")
     
-    query = 'SELECT * FROM course'
+    query = '''
+    WITH enroll AS (
+        SELECT course_id, COUNT(id) as enrollment
+        FROM takes
+        GROUP BY course_id
+    )
+
+    SELECT course_id, sec_id, title, credits, building, room_number, capacity, enrollment, day, start_hr, start_min, end_hr, end_min
+    FROM course
+    JOIN section USING(course_id)
+    JOIN classroom USING(room_number)
+    JOIN enroll USING(course_id)
+    JOIN time_slot USING(time_slot_id)
+    WHERE semester LIKE :sem AND year LIKE :year
+    ORDER BY course_id ASC
+    ;'''
 
 ### Register a student for a course
 def register_student():
+
+    # Connect to database
+    conn = psycopg2.connect(dbname="what")
+    cur = conn.cursor()
+    
     sem = input("Enter the semester: ")
     year = input("Enter the year: ")
     student_id = input("Enter the student ID: ")
     course_id = input("Enter the course ID: ")
     section_id = input("Enter the section ID: ")
 
+    query = '''
+    INSERT INTO TAKES VALUES (%s, %s, %s, %s, %s)
+    ;'''
+
+    try:
+        cur.execute(query, (sem,year,student_id,course_id, section_id))
+        conn.commit()
+    except psycopg2.errors.ForeignKeyViolation:
+        print("No such ID.")
+        conn.rollback()
+    finally:
+        conn.close
 
 ### Menu
 
@@ -89,25 +143,23 @@ print("\t--Input 4 to generate the course list")
 print("\t--Input 5 to register a student for a course")
 
 # Get user input
-valid_choice = False
+terminated = False
+quit_command_list = ("quit", "q", "Quit", "QUIT", "Q", "exit", "Exit", "EXIT")
 
-while (not valid_choice):
+while (not terminated):
     action = input("Please input a number to indicate your selection: ")
 
     if action == 1:
-        valid_choice = True
         gen_advisor_list()
     elif action == 2:
-        valid_choice = True
         hire_instructor()
     elif action == 3:
-        valid_choice = True
         gen_transcript()
     elif action == 4:
-        valid_choice = True
         gen_course_list()
     elif action == 5:
-        valid_choice = True
         register_student()
+    elif quit_command_list.contains(action):
+        terminated = True
     else:
         print("Invalid selection. Please enter a single integer from 1 to 5.")
